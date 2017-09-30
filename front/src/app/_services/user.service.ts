2fa49b64cac3fe3bff07e2bed2a9f5f1f28c5514
Unknown <sablancom@unal.edu.co>
2017-09-29 23:00:33 -0500

    Lógica de registro añadida

--- front/src/app/_services/user.service.ts
+++ front/src/app/_services/user.service.ts
@@ -2,29 +2,37 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http,Response,Headers,RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map'
-
+import 'rxjs/add/operator/toPromise';
 import { User } from '../_models/user';
 
 @Injectable()
 export class UserService {
-
-  constructor(private http: Http) {  }
-  getById(user_id){
-        //let body = {user_id: user_id, product_id: product_id}
-        //let headers = new Headers({ 'Content-Type': 'application/json'});
-        //let options = new RequestOptions({ headers:headers });
-
-        return this.http.get('http://192.168.99.101:3000/users'+user_id, this.jwt()).map((response:Response) => { console.log(response.json());
-        response.json();
-        }); 
+    private headers = new Headers({'Content-Type': 'application/json'});
+    private usersUrl = 'http://192.168.99.101:3000/users';  // URL to web api
+    constructor(private http: Http) {  }
+    getUsers(user_id){
+        return this.http.get(this.usersUrl)
+        .toPromise()
+        .then(response => response.json().data as User[])
+        .catch(this.handleError)
     }
-    private jwt() {
-        // create authorization header with jwt token
-        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
-            return new RequestOptions({ headers: headers });
-        }
+    getUser(id: number): Promise<User> {
+      const url = `${this.usersUrl}/${id}`;
+      return this.http.get(url)
+        .toPromise()
+        .then(response => response.json().data as User)
+        .catch(this.handleError);
     }
-
+    create(firstName: string, lastName: string, email: string, password:string): Promise<User> {
+      return this.http
+        .post(this.usersUrl, JSON.stringify({firstName: firstName, lastName: lastName, email:email, password:password }), {headers: this.headers})
+        .toPromise()
+        .then(res => res.json().data as User)
+        .catch(this.handleError);
+    }
+    
+    private handleError(error: any): Promise<any> {
+      console.error('An error occurred', error); // for demo purposes only
+      return Promise.reject(error.message || error);
+    }
 }
--- front/src/app/register/register.component.ts
+++ front/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+    users: User[];
+  constructor(private userService: UserService,
+    private router: Router) { }
 
   ngOnInit() {
   }
+    
+    add(firstName: string, lastName: string, email: string, password: string): void {
+      firstName = firstName.trim();
+      lastName = lastName.trim();
+      email = lastName.trim();
+      password = lastName.trim();
+      if (!firstName || !lastName || !email || !password) { return; }
+      this.userService.create(firstName, lastName, email, password).then(user => {
+        this.users.push(user);
+      });
+    }
 
 }
