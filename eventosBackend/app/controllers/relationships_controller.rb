5f6257dd8a8c324a9adfbfd206edcede92965f65
Unknown <sergioc11@hotmail.com>
2017-09-30 13:52:54 -0500

    Cambios routes

--- eventosBackend/app/controllers/relationships_controller.rb
+++ eventosBackend/app/controllers/relationships_controller.rb
@@ -39,6 +39,12 @@ class RelationshipsController < ApiController
     @relationship.destroy
   end
 
+def invite
+  #@cu = User.self 
+  #@cu.invite
+  render json: {m: "Hecho", id: params[:id]}
+end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_relationship
