8782001ebdae7bf2139a71ede105afe1769652ab
Luis Enrique Alfonso Rubio <luealfonsoru@unal.edu.co>
2017-10-02 19:41:05 -0500

    Vistas Actualizadas

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AddEventComponent } from './addEvent/addEvent.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login',  component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'addevent', component: AddEventComponent } 
+  { path: 'addevent', component: AddEventComponent },
+  { path: 'home', component: HomeComponent } 
 ];
 
 @NgModule({
